9077f6d9208401dbffa5c6610586fc3a37724131
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-08-23 16:04:57 +0300

    Update funcs.php

--- funcs.php
+++ funcs.php
@@ -1,24 +1,31 @@
 <?
-/*Получаем необходимые данные и передаем в файл*/
+//автоматизируем экранирование данных
+function clear() {
+  global $db;
+    foreach ($_POST as $key => $value) {
+      $_POST[$key] = mysqli_real_escape_string($db, $value);
+    }
+}
+
+/*Получаем данные и передаем в БД*/
 function save_mess() {
-  $str = $_POST['name_guest'] . " | " . $_POST['text_guest'] . " | " . date('Y-m-d H:i:s') . "\n***\n";
-  file_put_contents(GB,  $str, FILE_APPEND);
+  global $db;
+  clear();
+  extract($_POST); 
+  $insert_query = "INSERT INTO `guestbook_table` (`id`, `name`, `text`, `date`) VALUES (NULL, '$name_guest', '$text_guest', CURRENT_DATE())";
+  mysqli_query($db, $insert_query);
 }
 
-/*Вызывем полученный результат*/
+/*Получаем из БД массив*/
 function get_mess() {
-  return file_get_contents(GB);
+  global $db;
+  $insert_select = "SELECT * FROM `guestbook_table` ORDER BY id DESC";
+  $rez = mysqli_query($db, $insert_select);
+  return mysqli_fetch_all($rez, MYSQLI_ASSOC);
 }
 
-/*Разбиваем строку в массив*/
-function arry_mess($str_mess) {
-  $str_mess = explode("\n***\n", $str_mess);
-  array_pop($str_mess);//убираем последний элемент 
-  return array_reverse($str_mess);//сотрируем массив по убыванию
+//Функция для удобного просмотра массивов 
+function printArr($arr) {
+  echo '<pre>' . print_r($arr, true) . '</pre>';
 }
-
-function getFotmatMessages($str) {
-  return explode(" | " , $str);
-}
-
 ?>
